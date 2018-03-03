6fba9b8ab4805a860ad18947c9b1f41a75acd57c
Justin Huynh <polar.truth@gmail.com>
2018-03-03 16:28:27 -0500

    Add files via upload

--- build/src/CityPools.java
+++ build/src/CityPools.java
@@ -1,5 +1,49 @@
+import com.github.filosganga.geogson.gson.GeometryAdapterFactory;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
+/*
+Justin Huynh
+7745112
+Comprehensive Assignment
+ */
+
 public class CityPools {
-    List<Pool> pools;
+
+    public static void main(String[]args){
+        sortPools();
+    }
+
+    //Sorts the pools from west to east
+    public static void sortPools() {
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapterFactory(new GeometryAdapterFactory())
+                .create();
+        try {
+            Pool[] pools = gson.fromJson(new FileReader("wading-pools-min.json"), Pool[].class);
+            //SortbyLongitude sortLong = new SortbyLongitude();
+            Arrays.sort(pools);
+            for (int i = 0; i < pools.length; i++)
+                System.out.println(pools[i]);
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found!");
+        }
+    }
+    public static double euclidDistance(double lat1, double lon1, double lat2, double lon2){
+       double rLat1 = Math.toRadians(lat1);
+       double rLat2 = Math.toRadians(lat2);
+       double rLon1 = Math.toRadians(lon1);
+       double rLon2 = Math.toRadians(lon2);
+       double dRadians;
+
+       dRadians = 2*Math.asin(Math.sqrt(Math.pow((Math.sin((rLat1 - rLat2)/2)),2) + (Math.cos(rLat1) * Math.cos(rLat2) * Math.pow(Math.sin((rLon1 - rLon2)/2),2))));
+
+       return 6371.0 * dRadians;
+   }
+
 }
--- build/src/Pool.java
+++ build/src/Pool.java
@@ -1,12 +1,20 @@
 /**
  * Created by justi_000 on 2018-03-01.
  */
-public class Pool {
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Pool implements Comparable<Pool> {
     //public String NAME;
     public Properties properties;
     public Geometry geometry;
-    public Double[] coordinates;
+    //public Double[] coordinates;
 
+    public int compareTo(Pool other){
+        return this.geometry.coordinates[0].compareTo(other.geometry.coordinates[0]);
+    }
     public String toString(){
         return "'" + properties.NAME + "'" + "," + "[" + geometry.coordinates[0] + ", "
                 + geometry.coordinates[1] + "]";
